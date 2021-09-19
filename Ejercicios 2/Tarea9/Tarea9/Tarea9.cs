d58ae2c9782725a9e85ec88c4d7c1c3c004a8b27
Pablo Martín <pgmartin83@gmail.com>
2021-09-19 17:23:57 -0300

    9

---
+++ Ejercicios 2/Tarea9/Tarea9/Tarea9.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tarea9
+{
+    public class Tarea9
+    {
+        public static void Main()
+        {
+            string contraseña, contraseña2;
+            string nombre;
+            
+
+            do
+            {
+                Console.Write("Ingrese su nombre de usuario: ");
+                nombre = Console.ReadLine();
+                Console.Write("Ingrese su contraseña: ");
+                contraseña = Console.ReadLine();
+                Console.Write("Ingrese su contraseña nuevamente: ");
+                contraseña2 = Console.ReadLine();
+
+                if (contraseña != contraseña2)
+                    Console.WriteLine("Sus contraseñas no coinciden");
+            }
+            while (contraseña != contraseña2);
+
+            Console.WriteLine("Contraseña correcta");
+        }
+    }
+}
