c001c7ff310506039724ca86a62c13316e26e098
Pablo Martín <pgmartin83@gmail.com>
2021-09-19 15:27:06 -0300

    2

---
+++ Ejercicios 2/Tarea2/Tarea2/Tarea2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tarea2
+{
+    public class Tarea2
+    {
+        public static void Main()
+        {
+            int numero, contador = 0, res;
+
+            Console.WriteLine("ingrese un número positivo: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            while (numero > 0)
+            {
+                //res = numero % 10;
+                contador++;
+                numero = numero / 10;
+
+            }
+            Console.Write("El numero ingresado tiene {0} digitos", contador);
+        }
+    }
+}
