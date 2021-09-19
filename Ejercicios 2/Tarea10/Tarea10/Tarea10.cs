2405d910c5f333c89bb8f341056138ca2fb9139c
Pablo Martín <pgmartin83@gmail.com>
2021-09-19 17:28:53 -0300

    10

---
+++ Ejercicios 2/Tarea10/Tarea10/Tarea10.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tarea10
+{
+    public class Tarea10
+    {
+        public static void Main()
+        {
+            string nombre, dato = "Martín";
+
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+                
+            if (nombre != dato)
+            {
+                Console.WriteLine("No te conozco!");
+            }
+            else
+            {
+                Console.WriteLine("Hola Martín");
+            } 
+                
+        }
+    }
+}
