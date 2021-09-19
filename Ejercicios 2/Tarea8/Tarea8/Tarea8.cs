68b4c21550930873221ba77d6fd7a21349085c76
Pablo Martín <pgmartin83@gmail.com>
2021-09-19 17:02:05 -0300

    8

---
+++ Ejercicios 2/Tarea8/Tarea8/Tarea8.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tarea8
+{
+    public class Tarea8
+    {
+        public static void Main()
+        {
+            char letra;
+            Console.WriteLine("Ingrese una letra: ");
+            letra = Convert.ToChar(Console.ReadLine());
+
+            switch (letra)
+            {
+                case 'a': 
+                    Console.WriteLine("La letra {0} es una vocal", letra);
+                    break;
+                case 'e': 
+                    Console.WriteLine("La letra {0} es una vocal", letra);
+                    break;
+                case 'i':
+                    Console.WriteLine("La letra {0} es una vocal", letra);
+                    break;
+                case 'o':
+                    Console.WriteLine("La letra {0} es una vocal", letra);
+                    break;
+                case 'u':
+                    Console.WriteLine("La letra {0} es una vocal", letra);
+                    break;
+                default: Console.WriteLine("La letra {0} es una consonante", letra);
+                    break;
+            }
+        }
+    }
+}
