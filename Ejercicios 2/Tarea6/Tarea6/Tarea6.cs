bd103647014612309d916e3ca993a67a9e061f01
Pablo Martín <pgmartin83@gmail.com>
2021-09-19 16:45:10 -0300

    6

---
+++ Ejercicios 2/Tarea6/Tarea6/Tarea6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tarea6
+{
+    public class Tarea6
+    {
+        public static void Main()
+        {
+            int a = 5, b, c;
+
+            Console.WriteLine("a = 5: {0}", a);
+            Console.WriteLine("b = ++a: {0}", b = ++a);
+            Console.WriteLine("c = a++: {0}", c = a++);
+            Console.WriteLine("b = b * 5: {0}", b = b * 5);
+            Console.WriteLine("a = a * 2: {0}", a = a * 2);
+        }
+    }
+}
