437973149e072b766e5bec8afed4193a398dcddf
Pablo Martín <pgmartin83@gmail.com>
2021-09-19 15:54:45 -0300

    4

---
+++ Ejercicios 2/Tarea4/Tarea4/Tarea4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tarea4
+{
+    public class Tarea4
+    {
+        public static void Main()
+        {
+            int contador = 1;
+
+            do
+            {
+                Console.WriteLine("{0}", contador);
+                contador++;
+            }
+            while (contador != 0);
+        }
+    }
+}
