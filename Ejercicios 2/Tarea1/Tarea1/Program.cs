42c43ede6d7e2b2550d3baf00ee82782a68de5c5
pgmartin83 <70178884+pgmartin83@users.noreply.github.com>
2021-09-19 14:27:59 -0300

    Add files via upload

---
+++ Ejercicios 2/Tarea1/Tarea1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tarea1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int identificador = 1234, contraseña = 1111;
+            int numero1, numero2;
+
+            do
+            {
+                Console.Write("Ingrese su identificador numérico: ");
+                numero1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingrese su contraseña numérica: ");
+                numero2 = Convert.ToInt32(Console.ReadLine());
+
+                if ((numero1 != identificador) || (numero2 != contraseña))
+                    Console.WriteLine("Identificador o contraseña no válida.");
+            }
+            while ((numero1 != identificador) || (numero2 != contraseña));
+
+            Console.Write("Identificador y contraseña aceptada");
+        }
+    }
+}
