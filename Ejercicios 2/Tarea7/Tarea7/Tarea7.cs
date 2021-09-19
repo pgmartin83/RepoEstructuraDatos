0d31ae26a2a88ad442ca7048ed4091e960d340e2
Pablo Martín <pgmartin83@gmail.com>
2021-09-19 16:50:40 -0300

    7

---
+++ Ejercicios 2/Tarea7/Tarea7/Tarea7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tarea7
+{
+    public class Tarea7
+    {
+        public static void Main()
+        {
+            int a = 5, b, c;
+
+            Console.WriteLine("a=5: {0}", a);
+            Console.WriteLine("b=a+2: {0}", b=a+2);
+            Console.WriteLine("b-=3: {0}", b-=3);
+            Console.WriteLine("c=-3: {0}", c=-3);
+            Console.WriteLine("c*=2: {0}", c*=2);
+            Console.WriteLine("++c: {0}", ++c);
+            Console.WriteLine("a*=b: {0}", a*=b);
+        }
+    }
+}
