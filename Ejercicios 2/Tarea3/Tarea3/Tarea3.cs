26ab9d57b1e98f508389aaf0b6e3a3206a21abc8
Pablo Martín <pgmartin83@gmail.com>
2021-09-19 15:48:46 -0300

    3

---
+++ Ejercicios 2/Tarea3/Tarea3/Tarea3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tarea3
+{
+    public class Tarea3
+    {
+        public static void Main()
+        {
+            int contador = 1;
+
+            do
+            {
+                Console.WriteLine("9 x {0} = {1}", contador, 9 * contador);
+                contador++;
+            }
+            while (contador <= 10);
+            
+        }
+    }
+}
