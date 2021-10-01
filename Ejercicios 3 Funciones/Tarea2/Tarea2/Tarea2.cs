16bc688a46ed85f34243cd5e95948afd464eda25
Pablo Martín <pgmartin83@gmail.com>
2021-10-01 09:33:03 -0300

    2

---
+++ Ejercicios 3 Funciones/Tarea2/Tarea2/Tarea2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tarea2
+{
+    public class Tarea2
+    {
+        public static void Main()
+        {
+            int n1, n2;
+            Console.Write("Ingrese un número entero: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese otro número entero: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            Menor(n1, n2);
+        }
+
+        public static void Menor(int m1, int m2)
+        {
+            if (m1 < m2)
+                Console.Write("El menor es {0}", m1);
+            else
+                Console.Write("El menor es {0}", m2);
+
+        }
+    }
+}
