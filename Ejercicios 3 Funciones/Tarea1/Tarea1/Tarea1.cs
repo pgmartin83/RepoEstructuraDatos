8e60e300a652e4dd6d4b889563baeb8b57b73146
Pablo Martín <pgmartin83@gmail.com>
2021-10-01 09:21:27 -0300

    1

---
+++ Ejercicios 3 Funciones/Tarea1/Tarea1/Tarea1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tarea1
+{
+    public class Tarea1
+    {
+        public static void Main()
+        {
+            Single real;
+            Console.WriteLine("Ingrese un número real: ");
+            real = Convert.ToSingle(Console.ReadLine());
+            Signo(real);
+        }
+
+        public static void Signo(Single n)
+        {
+            if (n < 0)
+                Console.Write("-1");
+            else
+                Console.Write("1");
+        }      
+    }
+
+}
