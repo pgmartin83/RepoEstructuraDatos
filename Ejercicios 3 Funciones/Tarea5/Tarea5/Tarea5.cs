cc6745c252a5275c3a5fd5478397acc3be3ccf95
Pablo Martín <pgmartin83@gmail.com>
2021-10-12 13:56:17 -0300

    5

--- Ejercicios 3 Funciones/Tarea5/Tarea5/Tarea5.cs
+++ Ejercicios 3 Funciones/Tarea5/Tarea5/Tarea5.cs
@@ -8,9 +8,9 @@ namespace Tarea5
         {
             int o, n1, n2;
             Console.WriteLine("1. Suma");
-            Console.WriteLine("2. Suma");
-            Console.WriteLine("3. Suma");
-            Console.WriteLine("4. Suma");
+            Console.WriteLine("2. Resta");
+            Console.WriteLine("3. Multiplicación");
+            Console.WriteLine("4. División");
 
             Console.Write("Elija una opción: ");
             o = Convert.ToInt32(Console.ReadLine());
@@ -24,18 +24,24 @@ namespace Tarea5
             Calcula(o, n1, n2);
         }
         
-        public static int Calcula(int x, int a, int b)
+        public static float Calcula(int x, float a, float b)
         {
             switch (x)
             {
-                case 1: Sumar(a, b); break;
+                case 1: Console.WriteLine("calcula {0} + {1} = {2}", a, b, a + b); return 0;
+
+                case 2: Console.WriteLine("calcula {0} - {1} = {2}", a, b, a - b); return 0;
+
+                case 3: Console.WriteLine("calcula {0} * {1} = {2}", a, b, a * b); return 0;
+
+                case 4: Console.WriteLine("calcula {0} / {1} = {2}", a, b, a / b); return 0;
+
+                default:
+                    return 0;
+                    break;
             }
         }
         
-        public static int Sumar(int s1,int s2)
-        {
-            Console.WriteLine("calcula {0} + {1} = {2}", s1, s2, s1 + s2);
-        }
     }
 }
    
