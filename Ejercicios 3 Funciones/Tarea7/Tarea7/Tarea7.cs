1e57bd1356ed0a47e98266995ef4e08bf8c34eb9
Pablo Martín <pgmartin83@gmail.com>
2021-10-12 14:05:05 -0300

    7

---
+++ Ejercicios 3 Funciones/Tarea7/Tarea7/Tarea7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tarea7
+{
+    class Tarea7
+    {
+        static void Main()
+        {
+            Console.Write("Ingrese una letra: ");
+            string l = Console.ReadLine();
+            
+            Console.Write("Ingrese una numero: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = a; i >= 0; i--)
+            {
+                for (int b = 0; b < i; b++)
+                {
+                    Console.Write("{0}", l);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
