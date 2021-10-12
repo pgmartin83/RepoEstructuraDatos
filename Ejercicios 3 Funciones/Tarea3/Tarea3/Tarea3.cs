976d4e743d99adfab390ec40d5b8bdd8898ac6b9
Pablo Martín <pgmartin83@gmail.com>
2021-10-12 14:53:37 -0300

    3

--- Ejercicios 3 Funciones/Tarea3/Tarea3/Tarea3.cs
+++ Ejercicios 3 Funciones/Tarea3/Tarea3/Tarea3.cs
@@ -7,32 +7,36 @@ namespace Tarea3
         public static void Main()
         {
             int n;
-            Console.WriteLine("Escriba un número: ");
+            bool resultado;
+
+            Console.Write("Escriba un número: ");
             n = Convert.ToInt32(Console.ReadLine());
-            EsPrimo(n);
+
             
+
+            if (EsPrimo(n))
+            {
+                Console.WriteLine("El numero es primo");
+            }
+            else
+            {
+                Console.WriteLine("El numero no es primo");
+            }
         }
 
         static bool EsPrimo(int numero)
         {
-            int a = 0;
             for (int i = 2; i < numero; i++)
             {
                 if ((numero % i) == 0)
                 {
-                    a++;
+                    // No es primo :(
+                    return false;
                 }
+            }
 
-            }
-            if (a != 0)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-            
+            // Es primo :)
+            return true;
         }
     }
 }
