c94fa3940e65d160c65d7218fa58d778e6f2910e
Pablo Martín <pgmartin83@gmail.com>
2021-10-12 17:39:04 -0300

    Tarea 2

---
+++ TareaPila2/TareaPila2/TareaPila2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace TareaPila2
+{
+    public class TareaPila2
+    {
+        public static void Main()
+        {
+            double  numero;
+            Queue Cola = new Queue();
+
+            for (byte i = 0; i < 6; i++)
+            {
+                Console.Write("Ingrese un número entero: ");
+                numero = Convert.ToDouble(Console.ReadLine());
+                Cola.Enqueue(numero);
+            }
+
+            for (byte x = 0; x < 6; x++)
+            {
+                numero = (double)Cola.Dequeue();
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
