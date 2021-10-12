b37175771e10fab8b211a75c0cb14d86cd6567d8
Pablo Martín <pgmartin83@gmail.com>
2021-10-12 16:53:25 -0300

    punto 1

---
+++ TareaPila/TareaPila/TareaPila.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+namespace TareaPila
+{
+    public class TareaPila
+    {
+        public static void Main()
+        {
+            int numero;
+            Stack miPila = new Stack();
+            
+            for (byte i = 0; i < 6; i++)
+            {
+                Console.Write("Ingrese un número entero: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+                miPila.Push(numero);
+            }
+
+            for (byte x = 0; x < 6; x++)
+            {
+                numero = (int)miPila.Pop();
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
