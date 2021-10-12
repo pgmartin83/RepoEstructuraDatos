e782bfe6dccfcd0fc324a5fea71bfc9a9dac764f
Pablo Martín <pgmartin83@gmail.com>
2021-10-12 14:01:33 -0300

    6

---
+++ Ejercicios 3 Funciones/Tarea6/Tarea6/Tarea6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tarea6
+{
+    class Tarea6
+    {
+        static void Main()
+        {
+            string numeros;
+
+            Console.Write("Escribe un número: ");
+            numeros = Console.ReadLine();
+
+            Console.WriteLine("\nLa suma total de sus cifras es {0}.", SumaCifras(numeros));
+        }
+
+        public static int SumaCifras(string n)
+        {
+            int suma = 0;
+            for (int i = 0; i <= (n.Length - 1); i++)
+            {
+                suma += Convert.ToInt32(n[i].ToString());
+            }
+            return suma;
+        }
+    }
+}
