a921ae8606c10683762f7223de2b433bea74a2b5
Pablo Martín <pgmartin83@gmail.com>
2021-10-21 18:51:35 -0300

    Tarea3

---
+++ Pila y cola/Tarea3/Tarea3/Tarea3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace Tarea3
+{
+    class Tarea3
+    {
+        static void Main()
+        {
+            Stack pila = new Stack();
+            string frases;
+            int x = 0;
+
+            do
+            {
+                Console.Write("ingrese una frase: ");
+                frases = Convert.ToString(Console.ReadLine());
+
+                pila.Push(frases);
+                x++;
+
+            } while (frases != "");
+
+            for (int i = 0; i < x; i++)
+            {
+                frases = (string)pila.Pop();
+                Console.WriteLine(frases);
+
+            }
+        }
+    }
+}
