fc0053594268ff167052f49b05585ddae20f0eb6
Pablo Martín <pgmartin83@gmail.com>
2021-10-21 19:06:35 -0300

    Tarea3

--- Pila y cola/Tarea3/Tarea3/Tarea3.cs
+++ Pila y cola/Tarea3/Tarea3/Tarea3.cs
@@ -7,11 +7,30 @@ namespace Tarea3
     {
         static void Main()
         {
-            Stack pila = new Stack();
+            //Stack pila = new Stack();
+            Queue Cola = new Queue();
             string frases;
             int x = 0;
 
+            //Cola
             do
+            {
+                Console.Write("ingrese una frase: ");
+                frases = Convert.ToString(Console.ReadLine());
+
+                Cola.Enqueue(frases);
+                x++;
+
+            } while (frases != "");
+
+            for (int i = 0; i < x; i++)
+            {
+                frases = (string)Cola.Dequeue();
+                Console.WriteLine(frases);
+            }
+
+            //Pila
+            /*do
             {
                 Console.Write("ingrese una frase: ");
                 frases = Convert.ToString(Console.ReadLine());
@@ -26,7 +45,7 @@ namespace Tarea3
                 frases = (string)pila.Pop();
                 Console.WriteLine(frases);
 
-            }
+            }*/
         }
     }
 }
